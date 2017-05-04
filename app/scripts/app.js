06204b1441249c98b1e845541d5476f815459b4b
Tyler BoddySpargo <muyleche@gmail.com>
2017-05-03 20:37:06 -0500

    checkpoint 3 routing: complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,22 @@
 (function() {
-  angular.module('blocJams', []);
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('landing',{
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      }); }
+
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
 })();
